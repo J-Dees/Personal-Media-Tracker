f89f6c5667b1e9b83711fa8b1387e7572de212fb
Paul Motter <pmotter@calpoly.edu>
2024-11-02 14:07:43 -0700

    Added searching for all data_sets

--- src/api/server.py
+++ src/api/server.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from src.api import catalogs, entries, following, user_functions, admin
+from src.api import catalogs, entries, following, user_functions, admin, data_sets
 
 description = """ Track your stuff ! """
 
@@ -13,6 +13,7 @@ app.include_router(entries.router)
 app.include_router(following.router)
 app.include_router(user_functions.router)
 app.include_router(admin.router)
+app.include_router(data_sets.router)
 
 @app.get("/")
 async def root():
