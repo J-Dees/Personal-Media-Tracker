53adee649c57790481b818a24f6b6b3df6af68ba
Connor-Villanueva <ckvillan@calpoly.edu>
2024-11-03 00:13:27 -0700

    Finished all but searching

--- src/api/other_entries.py
+++ src/api/other_entries.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter
 from pydantic import BaseModel
 import sqlalchemy
 from src import database as db
+from datetime import date
 
 
 router = APIRouter(
@@ -11,10 +12,10 @@ router = APIRouter(
 
 class other_entries(BaseModel):
     title: str
-    opinion: str
-    rating: float
-    hours_played: float
-    play_again: bool
+    description: str
+    price: float
+    quality: str
+    date_obtained: date
     recommend: bool
     private: bool
 
@@ -36,17 +37,56 @@ def entry_exists(user_id: int, catalog_name: str, entry_title: str) -> bool:
             SELECT check_entry_exists(:catalog_name, :user_id, :entry_title) as verified
             """
         ), {"catalog_name": catalog_name, "user_id": user_id, "entry_title": entry_title}).first()
+    print(result.verified)
     return result.verified
 
 @router.post("/{user_id}/catalogs/{catalog_name}/other_entries")
-def create_game_entry(user_id: int, catalog_name: str, entry: other_entries):
+def create_other_entry(user_id: int, catalog_name: str, entry: other_entries):
     '''
     
     '''
     # insert into catalog table a new row with unqiue catalog id
     # do we want this to have a composite key for userid, catalog id, and entry id (ie user 1 catalog 1 entry 1, user 2 catalog 1 entry 1 etc)
 
-    # Later: check if catalog belongs to user, duplicates
+    try:
+        if (not catalog_belongs_to_user(user_id, catalog_name)):
+            raise Exception("Catalog does not belong to user.")
+
+        if (entry_exists(user_id, catalog_name, entry.title)):
+            raise Exception("Entry already exists.")
+        
+        with db.engine.begin() as connection:
+            entry_id = connection.execute(sqlalchemy.text(
+                """
+                INSERT INTO
+                    entries (catalog_id, private, recommend)
+                    (SELECT catalogs.id, :private, :recommend FROM catalogs WHERE name = :catalog_name)
+                RETURNING id
+                """
+            ), {
+                "private": entry.private,
+                "recommend": entry.recommend,
+                "catalog_name": catalog_name
+            }).one()
+
+            connection.execute(sqlalchemy.text(
+                """
+                INSERT INTO
+                    other_entry (entry_id, title, description, price, quality, date_obtained)
+                VALUES
+                    (:entry_id, :title, :description, :price, :quality, :date_obtained)
+                """
+            ), {
+                "entry_id": entry_id.id,
+                "title": entry.title,
+                "description": entry.description,
+                "price": entry.price,
+                "quality": entry.quality,
+                "date_obtained": entry.date_obtained
+            })
+    
+    except Exception as e:
+        print("Error:", e)
 
     return "OK"
 
@@ -57,20 +97,72 @@ def entry_search():
     return "OK"
 
 class update_other_entries(BaseModel):
-    opinion: str
-    rating: float
-    hours_played: float
-    play_again: bool
+    description: str
+    price: float
+    quality: str
 
 @router.put("/{user_id}/catalogs/{catalog_name}/other_entries/{entry_title}")
 def update_entry(user_id: int, catalog_name: str, entry_title: str, entry: update_other_entries):
     # update any value of the specified entry
+    print(entry_title)
+    try:
+        if (not catalog_belongs_to_user(user_id, catalog_name)):
+            raise Exception("Catalog does not belong to user.")
 
-    # Later, verify catalog belongs to user
-    
+        if (not entry_exists(user_id, catalog_name, entry_title)):
+            raise Exception("Entry does not exist.")
+        
+        with db.engine.begin() as connection:
+            parameters = entry.dict()
+            parameters.update({"entry_title": entry_title})
+            connection.execute(sqlalchemy.text(
+                """
+                UPDATE
+                    other_entry
+                SET
+                    description = :description,
+                    price = :price,
+                    quality = :quality
+                WHERE entry_id =
+                    (
+                        SELECT entry_id
+                        FROM entries
+                        JOIN other_entry ON other_entry.entry_id = entries.id
+                        WHERE other_entry.title = :entry_title
+                    )
+                """
+            ), parameters)
+
+    except Exception as e:
+        print("Error:", e)
     return "OK"
 
 @router.delete("/{user_id}/catalogs/{catalog_name}/other_entries/{entry_title}")
-def delete_entry():
+def delete_entry(user_id: int, catalog_name: str, entry_title: str):
     # DELETE FROM entries specified title
+
+    try:
+        if (not catalog_belongs_to_user(user_id, catalog_name)):
+            raise Exception("Catalog does not belong to user.")
+
+        if (not entry_exists(user_id, catalog_name, entry_title)):
+            raise Exception("Entry does not exist.")
+        
+        with db.engine.begin() as connection:
+            connection.execute(sqlalchemy.text(
+                """
+                DELETE FROM
+                    entries
+                WHERE id =
+                    (
+                        SELECT entry_id
+                        FROM entries
+                        JOIN other_entry ON other_entry.entry_id = entries.id
+                        WHERE other_entry.title = :entry_title
+                    )
+                """
+            ), {"entry_title": entry_title})
+    
+    except Exception as e:
+        print("Error:",e)
     return "OK"
