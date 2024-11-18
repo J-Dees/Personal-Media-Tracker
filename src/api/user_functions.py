5a266b8b990a08281ed9530fb92f704adc0bbbe9
J-Dees <jdees@outlook.com>
2024-11-17 17:40:51 -0800

    Update user_functions.py

--- src/api/user_functions.py
+++ src/api/user_functions.py
@@ -8,13 +8,16 @@ router = APIRouter(
     tags=["user functions"],
 )
 
-@router.post("/create")
+@router.post("")
 def create_new_user(name, response: Response):
     '''Creates a new user with the passed name and assigns a unique ID. If the user name is taken, the new user is informed to pick a different name.'''
     with db.engine.begin() as connection:
         try:
-            connection.execute(sqlalchemy.text("""INSERT INTO users (name)
-                                                VALUES (:name)"""), {'name': name})
+            connection.execute(sqlalchemy.text(
+                """
+                INSERT INTO users (name)
+                VALUES (:name)
+                """), {'name': name})
             response.status_code = status.HTTP_201_CREATED
             return "OK"
         except:
@@ -35,14 +38,17 @@ def create_new_user(name, response: Response):
             
 #             """))
 
-@router.get("/login")
+@router.get("")
 def login_user(name, response: Response):
     '''Allows the user to login with their username. If no such user exists, a message is sent to inform that no user exists by that name.'''
     with db.engine.begin() as connection:
         try:
-            user_id = connection.execute(sqlalchemy.text("""SELECT id
-                                                            FROM users
-                                                            WHERE name = :name"""), {'name' : name}).scalar_one()
+            user_id = connection.execute(sqlalchemy.text(
+                """
+                SELECT id
+                FROM users
+                WHERE name = :name
+                """), {'name' : name}).scalar_one()
             return {
                     "user_id": user_id
                     }
@@ -50,11 +56,19 @@ def login_user(name, response: Response):
             response.status_code = status.HTTP_404_NOT_FOUND
             return "User with provided username not found."
 
-@router.delete("/{user_id}")
-def delete_user(user_id):
+@router.delete("")
+def delete_user(user_id, response: Response):
     '''Deletes a user account. This will remove all traces of the user including catalogs, entries, and followers.'''
-    with db.engine.begin() as connection:
-        connection.execute(sqlalchemy.text("""DELETE FROM users
-                                              WHERE id = :user_id"""), {'user_id': user_id})
-    # TEST LATER to make sure it cascades
-    return "OK"
+    try:
+        with db.engine.begin() as connection:
+            connection.execute(sqlalchemy.text(
+                """
+                DELETE FROM users
+                WHERE id = :user_id
+                """), {'user_id': user_id})
+        # TEST LATER to make sure it cascades
+        response.status_code = status.HTTP_204_NO_CONTENT
+        return "Successfully deleted user account and all references."
+    except:
+        response.status_code = status.HTTP_404_NOT_FOUND
+        return "User with provided username not found"
